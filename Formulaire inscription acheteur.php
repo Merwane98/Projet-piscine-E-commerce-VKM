05572e5e591e3f0e0ff77841c514c781712eff5c
Merwane98 <47528533+Merwane98@users.noreply.github.com>
2019-04-30 14:51:54 +0200

    Update Formulaire inscription acheteur.php

--- Formulaire inscription acheteur.php
+++ Formulaire inscription acheteur.php
@@ -18,7 +18,6 @@
     if($nom == "") {$erreur .= "<p><b>Le champ Nom est vide. Merci de le remplir.</b><br></p>";}
     if($prenom == "") {$erreur .= "<p><b>Le champ Prénom est vide. Merci de le renseigner.</b><br></p>";}
     if($anniversaire == "") {$erreur .= "<p><b>Le champ Date de Naissance est vide. Merci de le renseigner.</b><br></p>";}
-    if($anniversaire >= '2001-04-30') {$erreur .= "<p><b>Le champ Date de Naissancerci de le renseigner.</b><br></p>";}
     if($telephone == "") {$erreur .= "<p><b>Le champ Telephone est vide. Merci de le renseigner.</b><br></p>";}
     if($adresse == "") {$erreur .= "<p><b>Le champ Adresse est vide. Merci de le renseigner.</b><br></p>";}
     if($cpostal == "") {$erreur .= "<p><b>Le champ Code postal est vide. Merci de le renseigner.</b><br></p>";}
