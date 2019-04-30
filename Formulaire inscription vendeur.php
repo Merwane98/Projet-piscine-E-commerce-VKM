1d76de9b00159aa76b0e853663cd391fce0d2031
Merwane98 <47528533+Merwane98@users.noreply.github.com>
2019-04-30 11:42:35 +0200

    Update Formulaire inscription vendeur.php

--- Formulaire inscription vendeur.php
+++ Formulaire inscription vendeur.php
@@ -13,19 +13,21 @@
     $mdp1 = isset($_POST["mdp1"])?$_POST["mdp1"] : "";
     $mdp2 = isset($_POST["mdp2"])?$_POST["mdp2"] : "";
  $erreur = "";
-    if($civilite == "") {$erreur .= "Le champ Civilité est vide. Merci de le renseigner <br>";}
-    if($nom == "") {$erreur .= " Le champ Nom est vide. Merci de le remplir <br>";}
-    if($prenom == "") {$erreur .= "Le champ Prénom est vide. Merci de le renseigner <br>";}
-    if($anniversaire == "") {$erreur .= "Le champ Date de Naissance est vide. Merci de le renseigner <br>";}
-    if($telephone == "") {$erreur .= "Le champ Telephone est vide. Merci de le renseigner <br>";}
-    if($adresse == "") {$erreur .= "Le champ Adresse est vide. Merci de le renseigner <br>";}
-    if($cpostal == "") {$erreur .= "Le champ Code postal est vide. Merci de le renseigner <br>";}
-    if($ville == "") {$erreur .= "Le champ Ville est vide. Merci de le renseigner <br>";}
-    if($mail1 == "") {$erreur .= "Le champ Adresse est vide. Merci de le renseigner <br>";}
-    if($mail2 != $mail1) {$erreur .= "Adresse mail de vérification différente de la précédente. Merci de le modifier <br>";}
-    if($mdp1 == "") {$erreur .= "Le champ Mot de passe est vide. Merci de le renseigner <br>";}
-    if($mdp2 != $mdp1) {$erreur .= "Mot de passe de vérification différent du précedent. Merci de le modifier <br>";}
-    if ($erreur == "") {echo "Bienvenue ".$prenom.", vous êtes bien inscrit ! <br>";}
+    if($anniversaire >= '2001-04-30') {$erreur .= "<p><b>Vous n'avez pas 18 ans. Merci de patienter jusqu'à votre majorité.</b><br></p>";}
+    if($civilite == ".") {$erreur .= "<p><b>Le champ Civilité est vide. Merci de le renseigner.</b><br></p>";}
+    if($nom == "") {$erreur .= "<p><b>Le champ Nom est vide. Merci de le remplir.</b><br></p>";}
+    if($prenom == "") {$erreur .= "<p><b>Le champ Prénom est vide. Merci de le renseigner.</b><br></p>";}
+    if($anniversaire == "") {$erreur .= "<p><b>Le champ Date de Naissance est vide. Merci de le renseigner.</b><br></p>";}
+    if($anniversaire >= '2001-04-30') {$erreur .= "<p><b>Le champ Date de Naissancerci de le renseigner.</b><br></p>";}
+    if($telephone == "") {$erreur .= "<p><b>Le champ Telephone est vide. Merci de le renseigner.</b><br></p>";}
+    if($adresse == "") {$erreur .= "<p><b>Le champ Adresse est vide. Merci de le renseigner.</b><br></p>";}
+    if($cpostal == "") {$erreur .= "<p><b>Le champ Code postal est vide. Merci de le renseigner.</b><br></p>";}
+    if($ville == "") {$erreur .= "<p><b>Le champ Ville est vide. Merci de le renseigner.</b><br></p>";}
+    if($mail1 == "") {$erreur .= "<p><b>Le champ Adresse est vide. Merci de le renseigner.</b><br></p>";}
+    if($mail2 != $mail1) {$erreur .= "<p><b>Adresse mail de vérification différente de la précédente. Merci de le modifier.</b><br></p>";}
+    if($mdp1 == "") {$erreur .= "<p><b>Le champ Mot de passe est vide. Merci de le renseigner.</b><br></p>";}
+    if($mdp2 != $mdp1) {$erreur .= "<p><b>Mot de passe de vérification différent du précedent. Merci de le modifier.</b><br></p>";}
+    if ($erreur == "") {echo "<p><b>Bienvenue ".$prenom.", votre compte vendeur vient d'être créé !<br>En vous souhaitant de bonnes ventes !</b></p>";}
  else {
  echo "Erreur : $erreur";
  }
